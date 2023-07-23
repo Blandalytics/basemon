b74fe0debcc2fcd8a9be361a4481d1d97994d0df
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2023-07-22 23:56:41 -0600

    Create basemon.py

---
+++ basemon.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import urllib
+from PIL import Image
+header = Image.open(urllib.request.urlopen('https://github.com/Blandalytics/hockeymon/blob/main/hockeymon%20logo.png?raw=true'))
+col1, col2, col3 = st.columns([0.1,0.8,0.1])
+with col1:
+    st.write(' ')
+
+with col2:
+    st.image(header)
+
+with col3:
+    st.write(' ')
+
+generations = ['Gen1','Gen2']
+poke_gen = st.radio('Choose a generation of Pokémon:', generations)
+@st.cache_data
+def load_players():
+    return list(pd.read_csv('https://github.com/Blandalytics/baseymon/blob/main/data/baseball_pokemon_Gen2.csv?raw=true', encoding='latin1')['full_name'].unique())
+players = load_players()
+
+# Player
+default_ix = players.index('Dustin May')
+player = st.selectbox('Select a player:', players, index=default_ix)
+
+card_loc = f"https://github.com/Blandalytics/basemon/blob/main/basemon_cards/{poke_gen}/{player.replace(' ','%20')}-{poke_gen}.png?raw=true"
+basemon_card = Image.open(urllib.request.urlopen(card_loc))
+st.image(basemon_card)
+
+st.write("Make sure to subscribe to [PitcherList Pro](https://www.pitcherlist.com/premium/) for more content like this!")
